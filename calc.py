6ea4bc4b56f69579ef36980e6e40c0a0be8bef5b
Vito <cuicvito@gmail.com>
2023-03-22 12:29:00 +0100

    some testing

--- calc.py
+++ calc.py
@@ -2,6 +2,7 @@ import json
 from datetime import timedelta
 import subprocess
 from datetime import datetime
+import time
 from numpy.random import shuffle
 from numpy.random import choice
 from os.path import abspath
@@ -37,9 +38,9 @@ def subset_sum(numbers, target, partial=[]):
             return r
    
 n = datetime.now()
-target_time = datetime(n.year, n.month, n.day, 13, 0, 0)
-# target = timedelta(minutes=20, seconds=20).total_seconds()
-target = (target_time - datetime.now()).total_seconds()
+target_time = datetime(n.year, n.month, n.day, 13, 30, 0)
+target = timedelta(hours=2).total_seconds()
+# target = (target_time - datetime.now()).total_seconds()
 
 if target < 0:
     print("Error: target_time can't be in the past")
@@ -49,7 +50,7 @@ if target < 0:
 MIN_LENGTH = timedelta(minutes=2).total_seconds()
 MAX_LENGTH = timedelta(minutes=5).total_seconds()
 
-FORCE_LAST_SONG = ["C:\\Users\\Vito\\Music\\Outer Wilds OST (.FLAC)\\10. End Times.flac"]
+FORCE_LAST_SONG = []
 
 if len(FORCE_LAST_SONG) > 0:
     LAST_SONG = choice(FORCE_LAST_SONG)
@@ -83,9 +84,22 @@ for song in data:
         break
 data = temp
 
+
+
+# for i in range(0,121,5):
+#     print(f"{i:3}: ",end='')
+#     for j in range(10):
+#         target = 60 * i
+#         shuffle(data)
+#         start_time = time.time()
+#         playlist = subset_sum(data, target)
+#         end_time = time.time() - start_time
+#         print(f"{end_time*1000:5.0f} ",end='')
+#     print("")
+
+# exit()
+
 shuffle(data)
-
-
 playlist = subset_sum(data, target)
 
 # Add forced last song
@@ -93,14 +107,14 @@ if LAST_SONG is not None:
     playlist.append(LAST_SONG)
 
 
-with open("playlist.m3u", "w", encoding='utf-8') as f:
-    f.write("#EXTM3U\n")
-    for song in playlist:
-        f.write(song["path"]+"\n")
+# with open("playlist.m3u", "w", encoding='utf-8') as f:
+#     f.write("#EXTM3U\n")
+#     for song in playlist:
+#         f.write(song["path"]+"\n")
 
 
 
-subprocess.Popen(["C:\Program Files (x86)\MusicBee\MusicBee.exe",f'{MAIN_PATH}\\playlist.m3u'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, startupinfo=startupinfo, creationflags=subprocess.CREATE_NO_WINDOW)
+# subprocess.Popen(["C:\Program Files (x86)\MusicBee\MusicBee.exe",f'{MAIN_PATH}\\playlist.m3u'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, startupinfo=startupinfo, creationflags=subprocess.CREATE_NO_WINDOW)
 
 print("Created playlist")
 print(f"\tEnd time: {target_time}\n\tLength: {str(timedelta(seconds=target)).split('.')[0]}\n\tMin song length: {timedelta(seconds=MIN_LENGTH)}\n\tMax song length: {timedelta(seconds=MAX_LENGTH)}")
